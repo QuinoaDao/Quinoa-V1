70f32ee74666b3015c87eb0977953e12a7a8a543
wakii <cwh6997@gmail.com>
2022-10-23 17:29:12 +0900

    add 1inch checkAllowance

---
+++ contract/src/1inchTest_checkAllowance.js
@@ -0,0 +1,36 @@
+import fetch from 'node-fetch';
+
+const swapParams = {
+    fromTokenAddress: "0x9c2C5fd7b07E95EE044DDeba0E97a665F142394f", // 1inch
+    toTokenAddress: "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063",// DAI
+    amount: '100000000000000000',
+    fromAddress: process.env.PUBLIC_ADDRESS,
+    slippage: 1,
+    disableEstimate: false,
+    allowPartialFill: false
+}
+
+const chainId = 137;
+
+const broadcastApiUrl = 'https://tx-gateway.1inch.io/v1.1/' + chainId + '/broadcast';
+const apiBaseUrl = 'https://api.1inch.io/v4.0/' + chainId;
+
+function apiRequestURL(methodName, queryParams) {
+    return apiBaseUrl + methodName + '?' + (new URLSearchParams(queryParams)).toString();
+}
+
+function checkAllowance(tokenAddress, walletAddress) {
+    return fetch(apiRequestURL('/approve/allowance', {tokenAddress, walletAddress}))
+    .then(res => res.json())
+    .then(res => res.allowance)
+}
+
+async function main() {
+    const allowance = await checkAllowance(swapParams.fromTokenAddress, process.env.PUBLIC_ADDRESS);
+    console.log(allowance);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
